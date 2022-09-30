da36329e545fd1faa4dab3434f4b3e40b81c3f72
mrsarkaw <sarkaw.salar55@gmail.com>
2022-09-30 15:27:21 +0300

    showing top photographer

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -10,7 +10,14 @@ class PublicController extends Controller
     public function index(){
 
         $newUser = User::latest()->limit(10)->get();
+        $topPhotographer = User::limit(6)->with('posts', function($q){
+            $q->latest()->limit(2);
+        })->withCount('posts')->get();
 
-        return view('index',  compact('newUser'));
+       $topPhotographer = $topPhotographer->sortByDesc(function($q){
+          return  $q->count_post;
+        });
+
+        return view('index',  compact('newUser', 'topPhotographer'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,18 +25,31 @@
     <div >
         <p class="text-2xl font-bold">Top Photographers</p>
         <div class="grid grid-cols-3  gap-10 mt-3">
-            
-            <div class="p-3 w-full 2xl:w-9/12 shadow rounded-xl">
-                <div class="flex  justify-center relative space-x-3 pb-16">
-                    <img src="https://wallpaperfordesktop.com/wp-content/uploads/2022/03/Street-Wallpaper-1024x576.jpg" class="w-24  h-24 xl:w-5/12 xl:h-40 object-cover rounded-2xl" alt="">
-                    <img src="https://wallpaperaccess.com/full/6497051.jpg" class="w-24  h-24  xl:w-5/12  xl:h-40 object-cover rounded-2xl" alt="">
-                    <div onclick="showSide()"  class="absolute bottom-4">
-                        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80" class="object-cover p-[2px] w-16 h-16 rounded-full bg-white" alt="">
-                        <p class=" text-xl text-black font-bold text-center">lanya</p>
-                        <p class=" text-sm text-gray-400 font-bold text-center">hawler</p>
+            @foreach ($topPhotographer as $row)
+                <div class="p-3 w-full 2xl:w-9/12 shadow rounded-xl">
+                    <div class="flex  justify-center relative space-x-3 pb-16">
+                        @if (count($row->posts) > 0)
+                            @foreach ($row->posts as $row2)
+                                <img src="{{ asset('postimage/'.$row2->image[0]) }}" class="w-24  h-24 xl:w-5/12 xl:h-40 object-cover rounded-2xl" alt="">
+                            @endforeach
+                        @else
+                            <div class="w-24  h-24 xl:w-5/12 xl:h-40 object-cover rounded-2xl bg-gray-400"></div>
+                            <div class="w-24  h-24 xl:w-5/12 xl:h-40 object-cover rounded-2xl bg-gray-400"></div>
+                        @endif
+
+                        @if (count($row->posts) == 1)
+                            <div class="w-24  h-24 xl:w-5/12 xl:h-40 object-cover rounded-2xl bg-gray-400"></div>
+                        @endif
+
+                        <div onclick="showSide()"  class="absolute -bottom-1">
+                            <img  src="{{ $row->avatar != null ? asset('coverimage/'.$row->avatar): 'https://ionicframework.com/docs/demos/api/avatar/avatar.svg' }}" class="object-cover p-[2px] w-16 h-16 mx-auto rounded-full bg-white" alt="">
+                            <p class=" text-xl text-black font-bold text-center">{{ $row->name }}</p>
+                            <p class=" text-sm text-gray-400 font-bold text-center">{{ $row->location }}</p>
+                            <p class=" text-xs text-gray-300 font-bold text-center">Post: {{ $row->posts_count }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 
